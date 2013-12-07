9ccde401e47e43def4acb91e9bb04f5ea6adc339
Éric Bélanger <ericbelanger28@hotmail.com>
2013-12-07 11:43:27 -0500

    My second commit with Git

--- server/launcher.js
+++ server/launcher.js
@@ -1,3 +1,4 @@
-var server = require('./routing/server');
+var server;
+server = require('./routing/server');
 server.listen(process.env.PORT || 3185);
 console.log('Listening on port ' + (process.env.PORT || 3185));
--- server/routing/server.js
+++ server/routing/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var path = require('path');
 var server = express();
 
 server.get('/ping', function(req, res) {
 	require('../challenges/Ping/ping.endpoint')(req, res);
+});
+
+server.get('/', function(req, res) {
+	var aPath = path.resolve (__dirname, '../challenges/Ping/landing_page.html');
+	res.sendfile(aPath);
 });
 
 module.exports = server;
